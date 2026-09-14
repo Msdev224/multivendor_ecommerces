f4736c49569691746617ce7b5f6ead895fa03b01
MAMADOU SAIDOU BAH <msdev224@gmail.com>
2026-09-14 03:56:17 +0000

    basic setup, sahdcn ui, theme-provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import ThemeToggle from "@/components/shared/theme-toggle";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="font-bold">Welcome to the Course !</h1>
+    <div className="p-5 w-full min-h-screen">
+      <div className="w-full flex justify-end">
+        <ThemeToggle/>
+      </div>
+      <h1 className="font-barlow text-blue-500 font-bold">Bienvenue sur GoShop</h1>
+      <Button variant="destructive">Click Here</Button>
     </div>
   );
 }
